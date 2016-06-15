76dce7514d090ee74ef027c3bd085fbab98c1436
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-15 18:40:54 +0800

    Hide push information

--- scripts/push_docs.sh
+++ scripts/push_docs.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-
-if [ -n "GITHUB_API_KEY" ]; then
+n
     git add .
     git -c user.name="Travis CI" commit -m "Update docs"
-    git push -f https://$GITHUB_API_KEY@github.com/zhuhaow/tun2socks.git gh-pages 
+    git push --force --quite https://$GITHUB_API_KEY@github.com/zhuhaow/tun2socks.git gh-pages
 fi
