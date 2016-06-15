60abf21e91f30b499a461b7d1858733769048178
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-15 18:51:18 +0800

    Fix typo

--- scripts/push_docs.sh
+++ scripts/push_docs.sh
@@ -1,5 +1,7 @@
-n
+#!/bin/sh
+
+if [ -n "GITHUB_API_KEY" ]; then
     git add .
     git -c user.name="Travis CI" commit -m "Update docs"
-    git push --force --quite https://$GITHUB_API_KEY@github.com/zhuhaow/tun2socks.git gh-pages
+    git push --force --quiet https://$GITHUB_API_KEY@github.com/zhuhaow/tun2socks.git gh-pages
 fi
