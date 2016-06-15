f8c8af893a6428b2b3aad331dbb5855adc814967
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-15 18:02:23 +0800

    Fix identity in push

--- scripts/push_docs.sh
+++ scripts/push_docs.sh
@@ -3,5 +3,5 @@
 if [ -n "GITHUB_API_KEY" ]; then
     git add .
     git -c user.name="Travis CI" commit -m "Update docs"
-    git push -f -q https://zhuhaow:$GITHUB_API_KEY@github.com/zhuhaow/tun2socks gh-pages &2>/dev/null
+    git push -f -q https://$GITHUB_API_KEY:x-oauth-basic@github.com/zhuhaow/tun2socks gh-pages &2>/dev/null
 fi
